dfa704f2b00b3ff69561fe34c297fc8410ff96e9
raihan <raihanulmcse@mail.com>
2022-02-25 04:50:44 +0800

    Add additional info

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 document.getElementById("sixteenGB").addEventListener("click", function () {
-    // const memoryCost = document.getElementById("memory-cost");
-    // memoryCost.innerText = 200;
+	// const memoryCost = document.getElementById("memory-cost");
+	// memoryCost.innerText = 200;
 
-    // const bestPrice = document.getElementById("best-price").innerText;
-    // const memoryPrice = document.getElementById("memory-cost").innerText;
-    // const storagePrice = document.getElementById("storage-cost").innerText;
-    // const deliveryPrice = document.getElementById("delivery-cost").innerText;
+	// const bestPrice = document.getElementById("best-price").innerText;
+	// const memoryPrice = document.getElementById("memory-cost").innerText;
+	// const storagePrice = document.getElementById("storage-cost").innerText;
+	// const deliveryPrice = document.getElementById("delivery-cost").innerText;
 
-    // const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
+	// const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
 
-    // const total = document.getElementById("total-price");
-    // total.innerText = totalPrice;
-    updatePrice("memory-cost", 300);
+	// const total = document.getElementById("total-price");
+	// total.innerText = totalPrice;
+	updatePrice("memory-cost", 300);
 });
 
 // document.getElementById("eightGB").addEventListener("click", function () {
@@ -27,12 +27,11 @@ document.getElementById("sixteenGB").addEventListener("click", function () {
 // });
 
 function onClick(clickId, updateId, price) {
-    document.getElementById(clickId).addEventListener("click", function () {
-        updatePrice(updateId, price);
-        document.getElementById(clickId).style.backgroundColor = "cornflowerblue"
-    });
+	document.getElementById(clickId).addEventListener("click", function () {
+		updatePrice(updateId, price);
+		document.getElementById(clickId).style.backgroundColor = "cornflowerblue";
+	});
 }
-
 
 onClick("sixteenGB", "memory-cost", 300);
 onClick("eightGB", "memory-cost", 0);
@@ -40,39 +39,36 @@ onClick("ssd1", "storage-cost", 0);
 onClick("ssd2", "storage-cost", 200);
 
 function updatePrice(itemId, price) {
-    const memoryCost = document.getElementById(itemId);
-    memoryCost.innerText = price;
+	const memoryCost = document.getElementById(itemId);
+	memoryCost.innerText = price;
 
-    const bestPrice = document.getElementById("best-price").innerText;
-    const memoryPrice = document.getElementById("memory-cost").innerText;
-    const storagePrice = document.getElementById("storage-cost").innerText;
-    const deliveryPrice = document.getElementById("delivery-cost").innerText;
+	const bestPrice = document.getElementById("best-price").innerText;
+	const memoryPrice = document.getElementById("memory-cost").innerText;
+	const storagePrice = document.getElementById("storage-cost").innerText;
+	const deliveryPrice = document.getElementById("delivery-cost").innerText;
 
-    const totalPrice =
-        parseFloat(bestPrice) +
-        parseFloat(memoryPrice) +
-        parseFloat(storagePrice) +
-        parseFloat(deliveryPrice);
+	const totalPrice =
+		parseFloat(bestPrice) +
+		parseFloat(memoryPrice) +
+		parseFloat(storagePrice) +
+		parseFloat(deliveryPrice);
 
-    const total = document.getElementById("total-price");
-    total.innerText = totalPrice;
+	const total = document.getElementById("total-price");
+	total.innerText = totalPrice;
 }
 
-const fakeCode = "pHero";
-document.getElementById("apply-btn").addEventListener("click", function(){
-    const code = document.getElementById("promo-input").value;
-    if(code === fakeCode){
-        const total = document.getElementById("total-price");
-        let totalPrice = parseFloat(total.innerText);
+const wrongCode = "pHero";
+document.getElementById("apply-btn").addEventListener("click", function () {
+	const code = document.getElementById("promo-input").value;
+	if (code === wrongCode) {
+		const total = document.getElementById("total-price");
+		let totalPrice = parseFloat(total.innerText);
 
-        const discount = (totalPrice*20)/100;
+		const discount = (totalPrice * 20) / 100;
 
-        totalPrice = totalPrice - discount;
+		totalPrice = totalPrice - discount;
 
-        total.innerText = totalPrice;
-
-    } else{
-
-    }
-})
-
+		total.innerText = totalPrice;
+	} else {
+	}
+});
